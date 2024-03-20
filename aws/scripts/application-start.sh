038474cc82779be6d063c20df8cbb4bbfcbb1adc
mark-wireman <108876042+mark-wireman@users.noreply.github.com>
2024-03-20 16:48:50 -0400

    Update application-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 set -xe
 
+prop="USERNAME"
+USERNAME="foobaruser"
+
+prop1="secretKey"
+secretKey="foobaruserkey"
+
+rcFile="~/.bashrc"
+
+if grep -q "^export $prop=" "$rcFile"; then
+  sed -i "s/^export $prop=.*$/export $prop=$USERNAME/" "$rcFile" &&
+  echo "[updated] export $prop"
+else
+  echo -e "export $prop=$USERNAME" >> "$rcFile"
+  echo "[inserted] export $prop"
+fi
+
+if grep -q "^export $prop1=" "$rcFile"; then
+  sed -i "s/^export $prop1=.*$/export $prop1=$secretKey/" "$rcFile" &&
+  echo "[updated] export $prop1"
+else
+  echo -e "export $prop1=$secretKey" >> "$rcFile"
+  echo "[inserted] export $prop1"
+fi
+
 # Start Tomcat, the application server.
 service tomcat start
